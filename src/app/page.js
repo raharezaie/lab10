a4d757f3de6138f2cacb679bd332a02f980cc47f
raharezaie <152831838+raharezaie@users.noreply.github.com>
2023-12-03 23:32:40 -0500

    page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,59 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// pages/index.js
+import Head from 'next/head'
+import styles from 'src/app/page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+    <div className={styles.container}>
+      <Head>
+        <title>Next.js Web Page</title>
+        <meta name="description" content="A simple Next.js web page" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Raha's Next.js Page
+        </h1>
+
+        <p className={styles.description}>
+          This page was created as a simple demonstration of a Next.js application.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+
+        <div className={styles.grid}>
+          <a href="#installation" className={styles.card}>
+            <h2>Installation &rarr;</h2>
+            <p>
+  To install Next.js, I first installed Node.js and npm. Then, I ran the command 
+  <code>npx create-next-app my-app</code> to create a new Next.js application. After the 
+  installation was complete, I navigated to the project directory and started the development 
+  server with <code>npm run dev</code>.
+</p>
+          </a>
+
+          <a href="#build" className={styles.card}>
+            <h2>Building the Page &rarr;</h2>
+            <p>
+  The page was built by editing the default <code>index.js</code> file created by Next.js. 
+  I used standard HTML for content and CSS modules for styling. The page is structured into 
+  main content and footer sections, with links to different sections of the page explaining the process.
+</p>
+          </a>
+
+          <a href="#difficulties" className={styles.card}>
+            <h2>Difficulties &rarr;</h2>
+            <p>
+  I encountered a difficulty with CSS styling initially, as it was not applying as expected. I solved this 
+  by ensuring that the CSS module was correctly imported into my page component. Another challenge was 
+  understanding server-side rendering and static generation, which I overcame by thoroughly reading the 
+  Next.js documentation and experimenting with different data fetching methods.
+</p>
           </a>
         </div>
-      </div>
+      </main>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className={styles.footer}>
+        Built with Next.js
+      </footer>
+    </div>
   )
 }
